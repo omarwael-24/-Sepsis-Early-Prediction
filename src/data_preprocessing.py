e22b8fed6cb9c06736d633720f1641267d3fe07c
Omar <omarelmaru@gmail.com>
2026-06-25 03:06:52 +0300

    add: add features, clean some columns

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -19,16 +19,31 @@ class DropUnwantedColumns:
 
     @staticmethod
     def drop_unwanted_columns(df: pd.DataFrame) -> pd.DataFrame:
-        unwanted_columns = ['Unit1', 'Unit2', 'HospAdmTime', 'Unnamed: 0']
+        unwanted_columns = ['Unit1', 'Unit2', 'HospAdmTime', 'Unnamed: 0'
+                            ,'Hour' ,'True_Hour']
         df = df.drop(columns=unwanted_columns, errors='ignore')
         return df
+    
+
 class SepsisDataProcessor:
 
     def __init__(self):
-        pass
+            pass
+    
     @staticmethod
     def SetMultIndexGroup(df: pd.DataFrame) -> pd.DataFrame:
         df['Spesis_onset'] = df.groupby('Patient_ID')['SepsisLabel'].diff()
         df = df.set_index(['Patient_ID','ICULOS'])
+        return df
+
+    @staticmethod
+    def impute_missing_values(df: pd.DataFrame) -> pd.DataFrame:
+        features_to_impute = config.Config.VITAL_SIGNS + config.Config.LAB_VALUES
+        
+        for col in features_to_impute:
+            df[f'{col}_is_missing'] = df[col].isna().astype(int)
+
+        df[features_to_impute] = df.groupby(level='Patiend_ID')[features_to_impute].ffill().bfill()
+
         return df
     
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,6 +1,8 @@
 """ ==================== TEMPORAL FEATURE ENGINEERING ==================== """
 import pandas as pd 
 import numpy as np 
+from src import config
+
 
 class HourFeature:
     def __init__(self):
@@ -12,3 +14,30 @@ class HourFeature:
         df['Hour_sin'] = np.sin(2 * np.pi * df['True_Hour'] / 24)
         df['Hour_cos'] = np.cos(2 * np.pi * df['True_Hour'] / 24)
         return df
+    
+class ExtractingRollingFeatures:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def extracting_rolling_features(df: pd.DataFrame) -> pd.DataFrame:
+        Vital_cols=config.Config.VITAL_SIGNS
+
+        groupd=df.groupby(level='Patient_ID')[Vital_cols]
+
+        df_rolling_mean= groupd.rolling(window=6,min_periods=1).mean().reset_index(level=0,drop=True)
+        
+        df_rolling_std= groupd.rolling(window=6,min_periods=1).std().fillna(0).reset_index(level=0,drop=True)
+
+        for col in Vital_cols:
+            df[f'{col}_roll_mean_6h'] = df_rolling_mean[col]
+            df[f'{col}_roll_std_6h'] = df_rolling_std[col]
+
+            df[f'{col}_delta_6h'] = df[col] - df[f'{col}_roll_mean_6h']
+        
+        df['qSOFA_score'] = ((df['Resp'] >= 22).astype(int) + (df['SBP'] <= 100).astype(int))
+        df['Long_Stay_Danger'] = (df['ICULOS'] > 48).astype(int)
+
+        return df
+    
